25f25854862c741158c3b4a703fa3077ad31a281
Camilo González <cgonzalezm1234@gmail.com>
2022-08-22 08:46:19 -0400

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Camilo Gonzalez on 22-08-22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
